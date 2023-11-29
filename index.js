5ca4c1af6bed37dcbd059dc5a40c33c652fd90b8
SajibBarua-art <sajibbaruanstu@gmail.com>
2023-11-29 23:32:56 +0600

    vercel adding

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ykirh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
